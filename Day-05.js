5de72f4177dc7a1ca5604934441cb1ff24435108
SheludkoOleh <104447016+SheludkoOleh@users.noreply.github.com>
2022-07-21 16:27:25 +0300

    Update Day-05.js

--- Day-05.js
+++ Day-05.js
@@ -1,1 +1,24 @@
 
+//https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+function bonusTime(salary, bonus) {
+  return "\u00A3" + (bonus ? salary * 10 : salary);
+}
+
+
+// https://www.codewars.com/kata/is-this-my-tail/train/javascript
+function correctTail(body, tail) {
+  const sub = body.substr(body.length - tail.length);
+
+  if (sub === tail) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// //https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/javascript
+function automorphic(n) {
+  return Math.pow(n, 2).toString().endsWith(n) ? "Automorphic" : "Not!!";
+}
