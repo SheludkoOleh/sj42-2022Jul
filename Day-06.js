61e10d1e1fce7a7bfd765c641190be99b7fd86b9
SheludkoOleh <104447016+SheludkoOleh@users.noreply.github.com>
2022-07-21 22:01:49 +0300

    Create Day-06.js

---
+++ Day-06.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/maximum-multiple/train/javascript
+
+function maxMultiple(divisor, bound) {
+  return bound - (bound % divisor);
+}
+
+
+
+
+//https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+function unusualFive() {
+  return "Oleg.".length
+}
