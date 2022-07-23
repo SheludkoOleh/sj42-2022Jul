f6790588b9dfcc345ba3cb5384eecb9349ff4bbe
SheludkoOleh <104447016+SheludkoOleh@users.noreply.github.com>
2022-07-23 10:46:28 +0300

    Update Day-06.js

--- Day-06.js
+++ Day-06.js
@@ -5,6 +5,11 @@ function maxMultiple(divisor, bound) {
 }
 
 
+//https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+function hero(bullets, dragons) {
+  return bullets >= dragons * 2;
+}
+
 
 
 //https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
