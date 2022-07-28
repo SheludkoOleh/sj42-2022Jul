779ae974225315e61141834e57ab3af12ee9675c
SheludkoOleh <104447016+SheludkoOleh@users.noreply.github.com>
2022-07-28 13:54:42 +0300

    Update Day-11.js

--- Day-11.js
+++ Day-11.js
@@ -7,3 +7,11 @@ function areEqual(s1, s2) {
 function notEqual(s1, s2) {
   return !areEqual(s1, s2);
 }
+
+//https://www.codewars.com/kata/5609fd5b44e602b2ff00003a/train/javascript
+function process2Arrays(arr1, arr2) {
+    const bothArr = [...new Set(arr1)].filter((value) => arr2.includes(value)).length;
+    const diffArr1 = [...new Set(arr1)].filter((value) => !arr2.includes(value)).length;
+    const diffArr2 = [...new Set(arr2)].filter((value) => !arr1.includes(value)).length;
+    return [bothArr, diffArr1 + diffArr2 , diffArr1, diffArr2];
+  }
