c5d1811b92cef40e9530d4a0f75458dda7761651
SheludkoOleh <104447016+SheludkoOleh@users.noreply.github.com>
2022-08-22 17:57:48 +0300

    Create Day-33.js

---
+++ Day-33.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/array-number-reduce/train/javascript
+
+
+// https://www.codewars.com/kata/52ed80dfe7bf9ae61d000056/train/javascript
+
+Object.deepFreeze = function (object) {
+  Object.freeze(object);
+  for (let i in object) {
+    Object.deepFreeze(object[i]);
+  }
+};
