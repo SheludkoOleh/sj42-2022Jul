ce5c9bf41478085bd027da57d04737db55578869
SheludkoOleh <104447016+SheludkoOleh@users.noreply.github.com>
2022-08-29 12:38:43 +0300

    Create Day-41.js

---
+++ Day-41.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/567ed5db4089538eea000010/train/javascript
+
+const launchAll = launchMissile => [0, 1, 2, 3, 4].forEach(launcher => setTimeout(() => launchMissile(launcher), launcher * 1000));
+
+//https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/javascript
+
+const queueTime = (customers, tillsAmount) => {
+  const activeTills = Array(tillsAmount).fill(0);
+  return customers.reduce((_, customer) => {
+    const fastest = activeTills.indexOf(Math.min(...activeTills));
+    activeTills[fastest] += customer;
+    const totalTime = Math.max(...activeTills);
+    return totalTime;
+  }, 0);
+}
